605096e2555e0de134ac75709846d0eaf360d009
peachykeenani <145533087+peachykeenani@users.noreply.github.com>
2024-01-28 23:56:57 -0600

    Loaded vehicles_us.csv file in module. Created new column 'manufacturer' from retrieving the first word in the 'model' column from the used cars dataset.

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+"""Module for Sprint 6 project"""
+
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+
+# Loaded the used cars dataset.
+df = pd.read_csv('vehicles_us.csv')
+"""Created new column 'manufacturer' \
+    by getting the first word from the 'model' column.
+    """
+df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
 
 st.header('Sprint 6 Project', divider='rainbow')
 
