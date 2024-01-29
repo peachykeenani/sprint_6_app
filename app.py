465340fa2df8f1b8824a680ef2e92004a94e91f7
peachykeenani <145533087+peachykeenani@users.noreply.github.com>
2024-01-29 01:59:44 -0600

    Added a project title and summary.

--- app.py
+++ app.py
@@ -6,12 +6,20 @@ import plotly.express as px
 
 # Loaded the used cars dataset.
 df = pd.read_csv('vehicles_us.csv')
-"""Created new column 'manufacturer' \
-    by getting the first word from the 'model' column.
-    """
+# Created new column 'manufacturer' 
+# by getting the first word from the 'model' column.
 df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
 
+# Added project title.
+st.title('Sprint 6 Project')
+# Added a project summary.
+st.write('This application refers to the vehicles_us.csv dataset.')
+# Added a divider.
+st.header('', divider='rainbow')
+st.header('')
+
 # Created a text header above the DataFrame.
-st.header('Data viewer', divider='rainbow')
+st.header('Data viewer')
 # Displayed the DataFrame with Streamlit.
 st.dataframe(df)
+st.divider()
